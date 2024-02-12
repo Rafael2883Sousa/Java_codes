2deb1bd9b2c499e84777fe81780b0e5890a143b8
Rafael Souza <rafs2883.1@gmail.com>
2024-02-12 10:05:40 -0100

    Not complete

---
+++ Programação Concorrente & Threads/Exame/src/Cozinheiros.java
@@ -0,0 +1,24 @@
+import java.util.concurrent.Semaphore;
+
+public class Cozinheiros implements Runnable{
+
+    public void run(){
+        /*for(int pedido : ListaPedidos){
+            
+            
+        }*/
+        while (pedidio) {
+            try {
+                Cozinheiro_1.tryAcquire();
+
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+            
+
+        }
+        
+    }
+    
+
+} 
