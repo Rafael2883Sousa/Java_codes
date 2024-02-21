d364a1005b30effab6c431fd45868013b33ee223
Rafael Souza <rafs2883.1@gmail.com>
2024-02-21 10:58:56 -0100

    Adding the synchronized method

--- Programação Concorrente & Threads/Syncronized/src/Syncronized1.java
+++ Programação Concorrente & Threads/Syncronized/src/Syncronized1.java
@@ -1,10 +1,13 @@
 public class Syncronized1 implements Runnable{
     
     private int i;
+    //static Object chave = new Object();
 
     public void run(){
-        i++;
-        System.out.println(Thread.currentThread().getName()+" -> "+i);
+        synchronized(this/*chave*/){
+            i++;
+            System.out.println(Thread.currentThread().getName()+" -> "+i);
+        }
     }
     
     public static void main(String[] args) {
@@ -14,9 +17,14 @@ public class Syncronized1 implements Runnable{
         Thread t0 = new Thread(sync);
         Thread t1 = new Thread(sync);
         Thread t2 = new Thread(sync);
+        Thread t3 = new Thread(sync);
+        Thread t4 = new Thread(sync);
+
 
         t0.start();
-        t1.start();
+        t1.start(); 
         t2.start();
+        t3.start();
+        t4.start();
     }
 }
